e6db58883d2dafed0f2234ef8e65fe5647e53379
belenamiune <amiunebelen88@gmail.com>
2024-09-20 12:28:37 -0300

    Add script and readme

---
+++ library_script.sql
@@ -0,0 +1,146 @@
+-- Created by belenamiune (https://github.com/belenamiune)
+
+DROP DATABASE IF EXISTS library;
+CREATE DATABASE library;
+
+USE library;
+-- Tables:
+
+-- Table: USER
+CREATE TABLE USER (
+    User_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    User_Name CHAR(100) NOT NULL,
+    Address VARCHAR(150),
+    Phone VARCHAR(15),
+    Email VARCHAR(100),
+    User_Type VARCHAR(50)
+);
+
+-- Table: LIBRARIAN
+CREATE TABLE LIBRARIAN (
+    Librarian_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Librarian_Name VARCHAR(100) NOT NULL,
+    Shift VARCHAR(50)
+);
+
+-- Table: CATEGORY
+CREATE TABLE CATEGORY (
+    Category_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Category_Name VARCHAR(100) NOT NULL,
+    Description TEXT
+);
+
+-- Table: EDITORY
+CREATE TABLE EDITORY (
+    Editory_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Editory_Name VARCHAR(100) NOT NULL
+);
+
+-- Table: SHELF
+CREATE TABLE SHELF (
+    Shelf_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Shelf_Number INT NOT NULL,
+    Section VARCHAR(50)
+);
+
+-- Table: AUTOR
+CREATE TABLE AUTOR (
+    Autor_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Autor_Name VARCHAR(100) NOT NULL
+);
+
+
+-- Table: BOOK
+CREATE TABLE BOOK (
+    Book_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Title VARCHAR(200) NOT NULL,
+    Year YEAR,
+    Amount_of_copies INT,
+    Category_ID INT,
+    Editory_ID INT,
+    Shelf_ID INT
+);
+
+-- Table: LOAN
+CREATE TABLE LOAN (
+    Loan_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Loan_Date DATE,
+    Return_Date DATE,
+    Status VARCHAR(50),
+    User_ID INT,
+	Librarian_ID INT
+);
+
+-- Table: RESERVATION
+CREATE TABLE RESERVATION (
+    Reservation_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Reservation_Date DATE,
+    Reservation_Status VARCHAR(50),
+    User_ID INT
+);
+
+-- Table: SANCTION
+CREATE TABLE SANCTION (
+    Sanction_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Sanction_Date DATE,
+    Price DECIMAL(10, 2),
+    Description TEXT,
+    Sanction_Status VARCHAR(50),
+    User_ID INT,
+    Loan_ID INT
+);
+
+-- Table: FINE
+CREATE TABLE FINE (
+    Fine_ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    Price DECIMAL(10, 2),
+    Description TEXT,
+    Sanction_ID INT
+);
+
+-- Definition of FK
+
+-- BOOK
+ALTER TABLE BOOK
+ADD CONSTRAINT FK_Book_Category
+FOREIGN KEY (Category_ID) REFERENCES CATEGORY(Category_ID);
+        
+ALTER TABLE BOOK
+ADD CONSTRAINT FK_Book_Editory
+FOREIGN KEY (Editory_ID) REFERENCES EDITORY(Editory_ID);
+        
+ALTER TABLE BOOK
+ADD CONSTRAINT FK_Book_Shelf
+FOREIGN KEY (Shelf_ID) REFERENCES SHELF(Shelf_ID);
+
+-- LOAN
+
+ALTER TABLE LOAN
+ADD CONSTRAINT FK_Loan_User
+FOREIGN KEY (User_ID) REFERENCES USER(User_ID);
+
+ALTER TABLE LOAN
+ADD CONSTRAINT FK_Loan_Librarian
+FOREIGN KEY (Librarian_ID) REFERENCES LIBRARIAN(Librarian_ID);
+
+-- RESERVATION
+ALTER TABLE RESERVATION
+ADD CONSTRAINT FK_Reservation_User
+FOREIGN KEY (User_ID) REFERENCES USER(User_ID);
+
+-- SANCTION
+ALTER TABLE SANCTION
+ADD CONSTRAINT FK_Sanction_User
+FOREIGN KEY (User_ID) REFERENCES USER(User_ID);
+
+ALTER TABLE SANCTION
+ADD CONSTRAINT FK_Sanction_Loan
+FOREIGN KEY (Loan_ID) REFERENCES LOAN(Loan_ID);
+
+
+-- FINE
+ALTER TABLE FINE
+ADD CONSTRAINT FK_Fine_Sanction
+FOREIGN KEY (Sanction_ID) REFERENCES SANCTION(Sanction_ID);
+
+-- End of file
